101be630ee615febdda9170a643b93816422ec76
Joseph Mueller <joseph.m.email@gmail.com>
2025-02-27 14:55:35 -0500

    imports_depens

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 
-import { QueryResult } from 'pg';
+import pg from 'pg';
 import { pool, connectToDb } from './connection.js';
+import inquirer from 'inquirer';
 
 await connectToDb();
 
-
+const promptUser = async () => {
+    const answer = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View Employees', 'Add Employee', 'Update Employee', 'Delete Employee', 'Exit']
+      }
+    ]);
+    
+    // Handle the selected action
+    console.log(`Selected action: ${answer.action}`);
+  };
+  
+  promptUser();
